d659278047276871419b73a271f56caf19a5848e
rdtejeda <roberto.daniel.tejeda@gmail.com>
2021-07-05 19:48:32 -0300

    ejercicio2fin

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,8 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-var nombredeusuario
-nombredeusuario=prompt("el nombre de usuario es")	
-	alert(nombredeusuario);
+var nombredeusuario;
+nombredeusuario=prompt("el nombre de usuario es");
+alert(nombredeusuario);
 }
-
