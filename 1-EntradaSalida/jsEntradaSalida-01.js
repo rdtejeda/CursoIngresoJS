0b277b29ddb4370ea0dd909fdad1b093901e7815
rdtejeda <roberto.daniel.tejeda@gmail.com>
2021-07-05 15:23:03 -0300

    Lunes 5 15hs

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola División J CI Julio2021");
+	alert("Hola División J 05Julio2021");
 }
 
