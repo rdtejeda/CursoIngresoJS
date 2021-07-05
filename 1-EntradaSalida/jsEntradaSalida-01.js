fbf4efcdcb18b002c6518fd69264ce560566b5a2
rdtejeda <roberto.daniel.tejeda@gmail.com>
2021-07-05 19:16:20 -0300

    presentado

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola División J 05Julio2021");
+	alert("Esto funciona de maravilla");
 }
 
