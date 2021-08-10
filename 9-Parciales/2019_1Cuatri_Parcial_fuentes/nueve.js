d14ba8c2ca52bd315723bea5711e03abcab698c6
rdtejeda <roberto.daniel.tejeda@gmail.com>
2021-08-10 16:21:45 -0300

    10ago

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,3 +1,126 @@
+
+/*Alumno Tejeda Roberto
+  DNI 18223125
+Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita ingresar , cantidad de habitantes en millones entre 1 y 7000 (validar)
+la temperaruta mínima que se registra en su territorio(entre -50 y 50) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de temperaturas pares.
+b) El nombre del pais con menos habitantes
+c) la cantidad de paises que superan los 40 grados.
+d) el promedio de habitantes entre los paises ingresados
+f) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura.
+
+
+*/
+function mostrar() {
+
+    var nombre;
+    var cantidad;
+    var temprom;
+    var seguir;
+    seguir = "si";
+    var cantidaddetpar;
+    cantidaddetpar = 0;
+    var banderamenoshabitantes;
+    banderamenoshabitantes = true;
+    var paismenoshabitantes;
+    var habitantesmenos;
+    var contadorpaisescalidos;
+    contadorpaisescalidos = 0;
+    var contadorhabitantes;
+    contadorhabitantes = 0;
+    var contadordepaises;
+    contadordepaises = 0;
+    var promhab;
+    var banderatempmin;
+    banderatempmin = true;
+    var paistempmin;
+    var tempminim;
+
+
+    while (seguir == "si") {
+
+        //nombre de pais
+        nombre = prompt("Ingrese Nombree de un país (Entre 3 y 10 caracteres");
+        while (nombre.length < 3 || nombre.length > 10) {
+            nombre = prompt("ERROR - Ingrese Nombree de un país (Entre 3 y 10 caracteres");
+        }
+        // cantidad de habitantes en millones entre 1 y 7000
+        cantidad = prompt("Ingrese cantidad de habitantes en millones entre 1 y 7000");
+        cantidad = parseInt(cantidad);
+        while (cantidad < 1 || cantidad >= 7000) {
+            cantidad = prompt("Ingrese cantidad de habitantes en millones entre 1 y 7000");
+            cantidad = parseInt(cantidad);
+        }
+        //la temperaruta mínima que se registra en su territorio(entre -50 y 50)
+        temprom = prompt("Ingrese la temperaruta mínima que se registra en su territorio(entre -50 y 50)");
+        temprom = parseInt(temprom);
+        while (temprom < -50 || temprom > 50) {
+            temprom = prompt("Ingrese la temperaruta mínima que se registra en su territorio(entre -50 y 50)");
+            temprom = parseInt(temprom);
+
+        }
+
+        if (temprom % 2 == 0)  //)a) La cantidad de temperaturas pares.
+        {
+            cantidaddetpar++
+        }
+        // b) El nombre del pais con menos habitantes
+        if (banderamenoshabitantes) {
+            banderamenoshabitantes = false;
+            paismenoshabitantes = nombre;
+            habitantesmenos = cantidad;
+        }
+        else {
+            if (cantidad < habitantesmenos) {
+                paismenoshabitantes = nombre;
+                habitantesmenos = cantidad;
+            }
+        }
+        // c) la cantidad de paises que superan los 40 grados.
+        if (temprom > 40) {
+            contadorpaisescalidos++
+        }
+        //   d) el promedio de habitantes entre los paises ingresados
+        contadordepaises++
+        contadorhabitantes = contadorhabitantes + cantidad;
+        
+        // f) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura.
+        if (banderatempmin) {
+            banderatempmin = false;
+            paistempmin = nombre;
+            tempminim = temprom
+        }
+        else {
+            if (temprom < tempminim) {
+                paistempmin = nombre;
+                tempminim = temprom
+            }
+        }
+        seguir = prompt("Para seguir ingrese 'si' y para terminar 'no'");
+        while (seguir != "si" && seguir != "no") {
+            seguir = prompt("ERROR - Para seguir ingrese 'si' y para terminar 'no'");
+        }
+
+    }
+    promhab = contadorhabitantes / contadordepaises;
+
+    alert(" a) La cantidad de temperaturas pares es " + cantidaddetpar);
+    alert(" b) El nombre del pais con menos habitantes es " + paismenoshabitantes);
+    alert(" c) la cantidad de paises que superan los 40 grados son " + contadorpaisescalidos);
+    alert(" d) el promedio de habitantes entre los paises ingresados es " + promhab);
+    alert(" f) la temperatura mínima ingresada es " + tempminim + " y nombre del pais que registro esa temperatura es " + paistempmin);
+
+}
+
+
+
+
+
+
+
+
 /*Enunciado:
 Bienvenidos.
 Realizar el algoritmo que permita ingresar el nombre de un país, cantidad de habitantes en millones entre 1 y 7000 (validar)
@@ -7,7 +130,6 @@ b) El nombre del pais con menos habitantes
 c) la cantidad de paises que superan los 40 grados.
 d) el promedio de habitantes entre los paises ingresados
 f) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura.
-*/
 
 function mostrar()
 {
@@ -40,18 +162,18 @@ while (seguir == "si")
 {
     pais=prompt("ingresar el nombre de un país");
     habitantes = prompt("cantidad de habitantes en millones entre 1 y 7000");
-	habitantes = parseInt(habitantes);
+    habitantes = parseInt(habitantes);
     while (habitantes<1 || habitantes>7000)
-	{
-		habitantes = prompt("INGRESE valor en millones entre 1 y 7000 ");
-		habitantes= parseInt(habitantes);		
-	}
+    {
+        habitantes = prompt("INGRESE valor en millones entre 1 y 7000 ");
+        habitantes= parseInt(habitantes);
+    }
     tempminima=parseInt(prompt("temperaruta mínima que se registra en su territorio(entre -50 y 50)"));
     while (tempminima<-50 || tempminima>50)
-	{
-		tempminima = prompt("INGRESE valor entre -50 y 50");
-		tempminima= parseInt(tempminima);		
-	}
+    {
+        tempminima = prompt("INGRESE valor entre -50 y 50");
+        tempminima= parseInt(tempminima);
+    }
    // a) La cantidad de temperaturas pares.
    if (tempminima%2==0)
    {
@@ -70,11 +192,11 @@ while (seguir == "si")
         {
             paismenoshabitado=pais;
         }
-    
+
     }
 
     //c) la cantidad de paises que superan los 40 grados.
-    if (tempminima>40) 
+    if (tempminima>40)
     {
         paisescalientes=paisescalientes+1;
     }
@@ -83,20 +205,20 @@ while (seguir == "si")
     paisestotales++;
     promediohab=habitantestotales/paisestotales;
 //f) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura.
-    if (banderatempminima) 
+    if (banderatempminima)
     {
         tempminimima=tempminima;
         paisfrio=pais;
-        banderatempminima=false;        
+        banderatempminima=false;
     }
     else
     {
-        if (tempminima<tempminimima) 
+        if (tempminima<tempminimima)
         {
         tempminimima=tempminima;
         paisfrio=pais;
-        }       
-        
+        }
+
     }
     seguir=prompt("para contibnuar la carga 'si'");
 }
@@ -105,3 +227,4 @@ document.write("La cantidad de temperaturas pares es "+canttempares+" ; El pais 
 " ; La cantidad de paises que superan los 40 grados es "+paisescalientes+" ; el promedio de habitantes entre los paises ingresados es "+promediohab+
 " ; la temperatura mínima ingresada es "+tempminimima+" y el pais que registro esa temperatura es"+paisfrio+"<br>");
 }
+*/
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,153 @@
+/*
+Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.
+
+Testeo con los siguientes datos
+( d , -150(mal) , -50(bien))
+( z , 0)
+( g , 20)
+( a , 150(mal) , 0(bien))
+( b , 3)
+( k , 7)
+*/
+
 function mostrar()
 {
+    var seguir;
+    seguir="si";
+    var letra;
+    var numero;
+    var cantidaddepares;
+    cantidaddepares=0;
+    var cantidaddeimpares;
+    cantidaddeimpares=0;
+    var cantidaddeceros;
+    cantidaddeceros=0;
+    var contarpositivos;
+    contarpositivos=0;
+    var sumarpositivos;
+    sumarpositivos=0;
+    var promediodepostivos;
+    var sumadenegativos;
+    sumadenegativos=0;
+    var banderamaximo;
+    banderamaximo=true;
+    var banderaminimo;
+    banderaminimo=true;
+    var numeromax;
+    var   letramax;
+    var numeromin;
+    var letramin;
 
+    while (seguir == "si")
+    {
+        //letra
+        letra= prompt("Ingrese una letra ");
+        while (letra.length<1 || letra.length>1) 
+          {
+            letra= prompt("ERROR Ingrese una letra");
+          }      
+        //Numero un número entre -100 y 100
+        numero = prompt("Ingrese  un número entre -100 y 100");
+        numero = parseInt(numero);
+        while( numero<-100 || numero>100)
+         {
+            numero = prompt("ERROR - Ingrese un número entre -100 y 100");
+            numero = parseInt(numero);    
+         }
+
+         //a) La cantidad de números pares.
+         if(numero%2 == 0)
+         {
+            cantidaddepares++
+         }
+         if (numero%2 == 1)
+         {
+           cantidaddeimpares++  
+         }
+         
+        //d) El promedio de todos los números positivos ingresados.
+         if (numero>0) 
+         {
+             contarpositivos++
+             sumarpositivos=sumarpositivos+numero;
+         } 
+         else
+             {
+                if (numero<0)// e) La suma de todos los números negativos.
+                {
+                    sumadenegativos=sumadenegativos+numero;
+                } 
+                else
+                {
+                     cantidaddeceros++ //c) La cantidad de ceros.
+                }
+             }
+      
+        if (banderaminimo) //f) El número y la letra del máximo y el mínimo.
+                   {
+                       banderaminimo=false;
+                       numeromin=numero;
+                        letramin=letra;
+                     
+                   } 
+                    else 
+                      {
+                          if (numero<numeromin) 
+                          {
+                            numeromin=numero;
+                            letramin=letra;
+                          }
+                      }
+
+             if (banderamaximo) 
+                   {
+                       banderamaximo=false;
+                       numeromax=numero;
+                       letramax=letra;
+                   } 
+                    else 
+                      {
+                          if (numero>numeromax) 
+                          {
+                            numeromax=numero;
+                            letramax=letra;
+                          }
+                      }
+
+     seguir=prompt("Para seguir ingrese 'si' y para terminar 'no'");
+     while (seguir!="si" && seguir!="no") 
+     {
+       seguir=prompt("ERROR - Para seguir ingrese 'si' y para terminar 'no'");
+     }
+    }
+promediodepostivos=sumarpositivos/contarpositivos;
+	alert("a) La cantidad de números pares ingresados fuero "+cantidaddepares);
+	alert("b) La cantidad de números impares es "+cantidaddeimpares);
+	alert("//c) La cantidad de ceros es "+cantidaddeceros);
+    alert("d) El promedio de todos los números positivos ingresados es "+promediodepostivos);
+    alert("e) La suma de todos los números negativos es "+sumadenegativos);
+    alert("f) El número y la letra del máximo son "+numeromax+" y "+letramax+" y el mínimo son "+numeromin+" y "+letramin);
+   // document.write("");
+
+  /*
+  if (var !=0)
+  {
+    alert("");
+  }
+  */
+  
+	/*
+	if (!(isNaN(var)))
+	{
+	alert("");
+	}
+	*/		
 }
